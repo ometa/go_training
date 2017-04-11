3e332bae8f69986dd47e45a65b88802b0234adf0
Devin Breen <dbreen@groupon.com>
2017-04-11 13:52:38 -0500

    Add in roman numerals

--- day_2/codingexamples/integer_translator/main/main.go
+++ day_2/codingexamples/integer_translator/main/main.go
@@ -3,15 +3,17 @@ package main
 import (
 	"fmt"
 	"github.com/ometa/go_training/day_2/codingexamples/integer_translator/fizzbuzz"
+	"github.com/ometa/go_training/day_2/codingexamples/integer_translator/rn"
+	"github.com/ometa/go_training/day_2/codingexamples/integer_translator/intconv"
 )
 
-func main() {
-
-	t := fizzbuzz.FizzBuzzer
-
-
+func runConverter(c intconv.Converter) {
 	for i := 1; i < 100; i++ {
-		result := t.Convert(i)
+		result := c.Convert(i)
 		fmt.Printf("[%s] %d - %s\n", result.Converter.Name, result.In, result.Out)
 	}
+}
+func main() {
+	runConverter(fizzbuzz.FizzBuzzer)
+	runConverter(rn.RNer)
 }
